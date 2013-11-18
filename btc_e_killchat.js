145db636977c2b5e3742dde8a283891e936eb63e
David Trail <napalm@trailbeans.eu>
2013-11-18 18:47:34 +0100

    Better

--- btc_e_killchat.js
+++ btc_e_killchat.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Remove chat from BTC-e
 // @namespace  http://trailbeans.eu/btc-e-chat-killer
-// @version    0.4
+// @version    0.8
 // @description  Removes superfluous crap from BTC-e.com
 // @match      https://*.btc-e.com/*
 // @copyright  2013+, David Trail <userscripts@trailbeans.eu>
@@ -9,4 +9,8 @@
 
 document.getElementById('nChatCon').parentNode.remove();
 document.getElementById('users-online').parentNode.remove();
-document.getElementById('twitter-widget-0').parentNode.remove();
+window.onload = function() {
+	document.getElementById('twitter-widget-0').parentNode.remove();
+};
+nChatPutMsg = function() { /* destroyed */ }
+nChatScroll = function() { /* destroyed */ }
