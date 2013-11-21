6535a66952adab2fb0d81eaeec267025893bd59f
David Trail <napalm@trailbeans.eu>
2013-11-21 18:53:21 +0100

    tidied up google ad removal a tad

--- google_ads_removal.js
+++ google_ads_removal.js
@@ -8,11 +8,12 @@
 // ==/UserScript==
 
 
-// Remove the cookies bar
+// Remove the cookies bar (uses click method, not sure if more correct would be to simply hide it).
 if (document.getElementById('epb-ok')) {
-    document.getElementById('epb-ok').click()
+    document.getElementById('epb-ok').click();
+	document.getElementById('epb-ok').remove();
 }
  
 for (var advert = document.getElementsByClassName('ads-container')[0]; document.getElementsByClassName('ads-container');) {
-    advert.remove()
+    advert.remove();
 }
