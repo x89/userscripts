dc5367c92aa2b6b077baa965e3e4afce44c43873
David Trail <napalm@trailbeans.eu>
2013-11-21 18:50:34 +0100

    Just another fuck you userscript.

---
+++ Fuck you Quora.tamper.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name       Fuck you Quora
+// @namespace  http://trailbeans.eu/quora
+// @version    0.1
+// @description  enter something useful
+// @match      http://*.quora.com/*
+// @copyright  2012+, You
+// ==/UserScript==
+
+$('.modal_signup_background').remove();
+$('.dialog_wrapper').remove();
