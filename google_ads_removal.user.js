eb29d5db1de3a14d8a306f1ea8167fdc998435ae
David Trail <napalm@trailbeans.eu>
2014-04-20 08:37:20 +0200

    google change?

--- google_ads_removal.user.js
+++ google_ads_removal.user.js
@@ -1,19 +1,23 @@
 // ==UserScript==
 // @name       Google's ads
 // @namespace  http://trailbeans.eu/google-ad-remover
-// @version    5
+// @version    6
 // @description  Remove the Google ads.
 // @match      https://*.google.es/search*
 // @copyright  2012+, You
 // ==/UserScript==
 
-
+sleep(500);
 // Remove the cookies bar (uses click method, not sure if more correct would be to simply hide it).
 if (document.getElementById('epb-ok')) {
     document.getElementById('epb-ok').click();
 	document.getElementById('epb-ok').remove();
 }
- 
-for (var advert = document.getElementsByClassName('ads-container')[0]; document.getElementsByClassName('ads-container');) {
-    advert.remove();
-}
+
+divs = [
+	'tads',
+	'rhs_block',
+]
+
+for (var i = 0; i < divs.length; i++)
+	document.getElementById(divs[i]).remove()
