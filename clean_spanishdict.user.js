f9c3828f3ed06e6bf684ba5690e82bc3bf2f43fb
David Trail <napalm@trailbeans.eu>
2014-05-16 13:08:41 +0200

    RAAAAAA

--- clean_spanishdict.user.js
+++ clean_spanishdict.user.js
@@ -1,22 +1,25 @@
 // ==UserScript==
 // @name        Clean up SpanishDict
-// @namespace   *.spanishdict.com
-// @include     http://www.spanishdict.com/*
-// @version     1
-// @grant       none
+// @description Removes the tam from SD
+// @namespace   http://trailbeans.eu/spanishdictcleaner
+// @match    	*://*.spanishdict.com/*
+// @version     3
 // ==/UserScript==
 
-document.getElementById('survey_container').remove();
+function run_clean() {
+	document.getElementById('survey_container').remove();
 
-var class_names = [
-  'fluencia-bar', // that shit at the top
-  'content', // For some reason they call "content" the sidebar
-  ];
+	var class_names = [
+		'fluencia-bar', // that shit at the top
+		'content', // For some reason they call "content" the sidebar
+	];
 
-for (var i = 0; i < class_names.length; i++) {
-  var classes = document.getElementsByClassName(class_names[i]);
-  for (var j = 0; j < classes.length; j++) {
-    classes[j].remove();
-  }
-  
+	for (var i = 0; i < class_names.length; i++) {
+		var classes = document.getElementsByClassName(class_names[i]);
+		for (var j = 0; j < classes.length; j++) {
+			classes[j].remove();
+		}  
+	}
 }
+
+setTimeout(run_clean, 150);
